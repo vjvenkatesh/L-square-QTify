2fed381b3d8f84ecbc2c64d0fc055f50f37a4a2b
vjvenkatesh <vjvenkatbsc@gmail.com>
2023-10-20 22:49:54 +0530

    clearing console commit

--- qtify/src/Api/api.js
+++ qtify/src/Api/api.js
@@ -8,7 +8,6 @@ export const BACKEND_ENDPOINT='https://qtify-backend-labs.crio.do';
 export const fetchTopAlbums=async()=>{
     try{
         const res=await axios.get(`${BACKEND_ENDPOINT}/albums/top`);
-        console.log(res.data);
         return res.data;
     }
     catch(e){
